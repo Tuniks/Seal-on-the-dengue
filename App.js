8e0a3a764a403b3948c02359145ec6c06fdc5c58
Tuniks <tonikpa@gmail.com>
2017-11-20 18:21:24 -0200

    removed ios only components

--- App.js
+++ App.js
@@ -13,33 +13,13 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.view}>
-          <TabBarIOS>
-              <TabBarIOS.Item
-                  systemIcon="favorites"
-                  title="map"
-                  selected={this.state.selectedTab === 0}
-                  onPress={this.handleTabPress.bind(this, 0)}
-              >
-                  <PlaceMap markers={this.state.markers}/>
-              </TabBarIOS.Item>
-
-              <TabBarIOS.Item
-                  title="places"
-                  icon={require('./assets/pin.png')}
-                  selected={this.state.selectedTab === 1}
-                  onPress={this.handleTabPress.bind(this, 1)}
-              >
-                  <View style={styles.view}>
-                      <Text style={styles.text}>Add Place</Text>
-                  </View>
-              </TabBarIOS.Item>
-          </TabBarIOS>
+          <PlaceMap/>
       </View>
     );
   }
-    handleTabPress(tab) {
-        this.setState({ selectedTab: tab })
-        }
+    // handleTabPress(tab) {
+    //     this.setState({ selectedTab: tab })
+    //     }
 }
 
 
@@ -51,9 +31,6 @@ const styles = StyleSheet.create({
     },
     view: {
         backgroundColor: '#fed',
-        flex: 1
-    },
-    map: {
         flex: 1
     }
 });
